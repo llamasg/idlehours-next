948f9479253a5fb7943c74dcd2ff9abce31edb7f
Alfie <llamasg98@gmail.com>
2026-02-22 01:32:16 +0000

    chore: add appId to sanity.cli.ts to skip hostname prompt on deploy

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -11,6 +11,7 @@ export default defineCliConfig({
      * To enable version-controlled updates, add an appId from:
      * https://www.sanity.io/manage/project/ijj3h2lj/studios
      */
+    appId: 'tsb3bjdq1gp4n843g7hn7t26',
     autoUpdates: false,
   }
 })
